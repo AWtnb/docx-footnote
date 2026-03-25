2e3ae370736675157850206260ab8fcaf3a5c8be
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-03-25 09:56:29 +0900

    update bat

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 @echo off
 chcp 65001 >nul
-rem Run the project using "uv run main.py"
 uv run main.py
 echo.
-echo 完了しました！ 終了するには何かキーを押してください
+echo OWARI MASHITA! (press any key to close)
 pause >nul
